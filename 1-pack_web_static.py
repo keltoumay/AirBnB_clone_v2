0e875f209b33f795f864b1f1c8a38e7f7ad1200d
keltoumy <keltoumaouyahia@gmail.com>
2024-02-04 15:04:42 -0800

    upv2

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
-"""pack all content within web_static
-"""
+"""Fabric script that generates a .tgz archive from the
+contents of the web_static
+folder of your AirBnB Clone repo, using the function do_pack."""
 from fabric.api import local
 from datetime import datetime
-import os
 
 
 def do_pack():
-    """pack all content within web_static
-    into a .tgz archive
-    The archive will be put in versions/
-    """
-    if not os.path.exists("versions"):
-        local("mkdir versions")
-    now = datetime.now()
-    name = "versions/web_static_{}.tgz".format(
-        now.strftime("%Y%m%d%H%M%S")
-    )
-    cmd = "tar -cvzf {} {}".format(name, "web_static")
-    result = local(cmd)
-    if not result.failed:
-        return name
+    """Function to generate a .tgz archive from the contents of the web_static folder."""
+    local("mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    path = "versions/web_static_{}.tgz".format(date)
+    try:
+        local("tar -cvzf {} web_static".format(path))
+        return path
+    except:
+        return None
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,50 +1,29 @@
 #!/usr/bin/python3
-"""pack and deploy content to server
-"""
-from fabric.api import local, env, run, put
-from datetime import datetime
-import os
-env.hosts = ['100.25.193.13','18.209.179.202']
+"""Fabric script that distributes an archive to your web servers,
+using the function do_deploy."""
+from fabric.api import put, run, env
+from os import path
+
+
+env.hosts = ['54.82.132.211', '100.25.194.199']
 env.user = 'ubuntu'
-
-
-def do_pack():
-    """pack all content within web_static
-    into a .tgz archive
-    The archive will be put in versions/
-    """
-    if not os.path.exists("versions"):
-        local("mkdir versions")
-    now = datetime.now()
-    name = "versions/web_static_{}.tgz".format(
-        now.strftime("%Y%m%d%H%M%S")
-    )
-    cmd = "tar -cvzf {} {}".format(name, "web_static")
-    result = local(cmd)
-    if not result.failed:
-        return name
-
+env.key_filename = "secret_key"
 
 def do_deploy(archive_path):
-    """deploy package to remote server
-    Arguments:
-        archive_path: path to archive to deploy
-    """
-    if not archive_path or not os.path.exists(archive_path):
+    """Function to distribute an archive to your web servers."""
+    if not path.exists(archive_path):
         return False
-    put(archive_path, '/tmp')
-    ar_name = archive_path[archive_path.find("/") + 1: -4]
     try:
-        run('mkdir -p /data/web_static/releases/{}/'.format(ar_name))
-        run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/'.format(ar_name, ar_name))
-        run('rm /tmp/{}.tgz'.format(ar_name))
-        run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(ar_name, ar_name))
-        run('rm -rf /data/web_static/releases/{}/web_static'.format(
-            ar_name
-        ))
-        run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(ar_name))
-        print("New version deployed!")
+        put(archive_path, "/tmp/")
+        name = archive_path.split("/")[-1]
+        name2 = name.split(".")[0]
+        run("mkdir -p /data/web_static/releases/{}".format(name2))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(name, name2))
+        run("rm /tmp/{}".format(name))
+        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(name2, name2))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(name2))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(name2))
         return True
     except:
         return False
