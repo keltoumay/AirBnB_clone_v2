33f9426c90e73bfa78e4f09b80b1f662da61894f
keltoumy <keltoumaouyahia@gmail.com>
2024-02-18 07:11:32 -0800

    web_flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-"""Start a flask web app
+"""
+start Flask application
 """
 
 from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_flask():
-    """Return string when route queried
-    """
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
-    app.url_map.strict_slashes = False
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
